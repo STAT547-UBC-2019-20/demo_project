07bedef44c3129a28818cf4c9765d6121dbe0225
Matt <matthew.at.connell@gmail.com>
2020-03-02 21:56:12 -0800

    adding pdf and .tex file

--- src/knit.R
+++ src/knit.R
@@ -1,2 +1,1 @@
-rmarkdown::render("../docs/milestone1-547-example.Rmd")#, c("html_document", "pdf_document"))
-rmarkdown::render("../docs/milestone1-547-example.Rmd", "pdf_document")
+rmarkdown::render("../docs/milestone1-547-example.Rmd", c("html_document", "pdf_document"))

4d7693154f2ed5a1d82cdaa67e77d419da4eb2b7
Firas Moosvi <firas@moosvi.com>
2020-03-09 03:10:03 -0700

    add commandline arguments to clean.R

--- src/clean.R
+++ src/clean.R
@@ -1,19 +1,29 @@
+"This script loads in the autism.csv file, cleans it, and then re-exports the file with the same name
+
+Usage: src/clean.R --data_filepath=<data_filepath>" -> doc
+
 library(tidyverse)
+library(docopt)
 
-## Load the csv
-df <- read_csv("../data/autism.csv")
+opt <- docopt(doc)
 
-# If you have columns you need to drop from your dataframe, try the following
-drop <- c("X1", "X2")
-df <- df[ , !(names(df) %in% drop)]
-## Attribution https://stackoverflow.com/questions/4605206/drop-data-frame-columns-by-name
+main <- function(data_filepath){
+    ## Load the csv
+    df <- read_csv(data_filepath)
 
+    # If you have columns you need to drop from your dataframe, try the following
+    drop <- c("X1", "X2")
+    df <- df[ , !(names(df) %in% drop)]
+    ## Attribution https://stackoverflow.com/questions/4605206/drop-data-frame-columns-by-name
 
-# Three of our column names are mispelled (jundice -> jaundice, austim -> autism, and contry_of_res)
-# Look at the column names with: names(df)
-names(df)[14] <- "jaundice"
-names(df)[15] <- "autism"
-names(df)[16] <- "country_of_res"
+    # Three of our column names are mispelled (jundice -> jaundice, austim -> autism, and contry_of_res)
+    # Look at the column names with: names(df)
+    names(df)[14] <- "jaundice"
+    names(df)[15] <- "autism"
+    names(df)[16] <- "country_of_res"
 
-# Save our cleaned dataframe
-write_csv(df, path = "../data/autism.csv")
+    # Save our cleaned dataframe
+    write_csv(df, path = data_filepath)
+}
+
+main(opt$data_filepath)
